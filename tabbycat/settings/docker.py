d0d90c3a60ca942f696c2a582b5e981bb52f8f7c
efemrgl <ahmetefemorgul@gmail.com>
2026-02-26 22:08:18 +0300

    Updatedd docker.py

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -6,16 +6,29 @@ import os
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tabbycat',
-        'USER': 'tabbycat',
-        'PASSWORD': 'tabbycat',
-        'HOST': 'db',
-        'PORT': 5432, # Non-standard to prevent collisions,
+import os
+import dj_database_url
+
+ALLOWED_HOSTS = ["*"]
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'tabbycat',
+            'USER': 'tabbycat',
+            'PASSWORD': 'tabbycat',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
 
 if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else False:
     CACHES = {
